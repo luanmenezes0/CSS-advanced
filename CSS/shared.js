18563ef33b6b53dd08ccc36ff78638ca681c7aa5
Luan <luanmenezes99@gmail.com>
2020-10-17 11:54:36 -0300

    finished

--- CSS/shared.js
+++ CSS/shared.js
@@ -1,52 +1,46 @@
-var backdrop = document.querySelector(".backdrop");
-var selectPlanButtons = document.querySelectorAll(".plan button");
-var modal = document.querySelector(".modal");
-var noButton = document.querySelector(".modal button");
-var toggleButton = document.querySelector(".toggle-button");
-var mobileNav = document.querySelector(".mobile-nav");
-
-console.dir(noButton);
+var backdrop = document.querySelector('.backdrop');
+var selectPlanButtons = document.querySelectorAll('.plan button');
+var modal = document.querySelector('.modal');
+var noButton = document.querySelector('.modal button');
+var toggleButton = document.querySelector('.toggle-button');
+var mobileNav = document.querySelector('.mobile-nav');
+var ctaButtton = document.querySelector('.main-nav__item--cta');
 
 function closeModal() {
-  // modal.style.display = "none";
-  // backdrop.style.display = "none";
-  backdrop.classList.remove("open");
+  backdrop.classList.remove('open');
   if (modal) {
-    modal.classList.remove("open");
+    modal.classList.remove('open');
   }
 }
 
-toggleButton.addEventListener("click", function () {
-  mobileNav.classList.add("open");
-  backdrop.classList.add("open");
+toggleButton.addEventListener('click', function () {
+  mobileNav.classList.add('open');
+  backdrop.classList.add('open');
 });
 
-backdrop.addEventListener("click", function () {
-  // mobileNav.style.display = "none";
-  mobileNav.classList.remove("open");
+backdrop.addEventListener('click', function () {
+  mobileNav.classList.remove('open');
   closeModal();
 });
 
 if (noButton) {
-  noButton.addEventListener("click", () => {
-    backdrop.classList.remove("open");
+  noButton.addEventListener('click', () => {
+    backdrop.classList.remove('open');
     if (modal) {
-      modal.classList.remove("open");
+      modal.classList.remove('open');
     }
   });
 }
 
 if (backdrop) {
-  backdrop.addEventListener("click", closeModal());
+  backdrop.addEventListener('click', closeModal());
 }
 
 for (let index = 0; index < selectPlanButtons.length; index++) {
-  selectPlanButtons[index].addEventListener("click", function () {
-    // modal.style.display = "block";
-    // backdrop.style.display = "block";
-    backdrop.classList.add("open");
+  selectPlanButtons[index].addEventListener('click', function () {
+    backdrop.classList.add('open');
     if (modal) {
-      modal.classList.add("open");
+      modal.classList.add('open');
     }
   });
 }
